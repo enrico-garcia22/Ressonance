271b7d923f2220ae7762de713d18ce39be498be1
Enrico de Castro Garcia <enrico.garcia@unesp.br>
2026-09-16 16:06:02 -0300

    Inicio do projeto Ressonance

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Seleciona os elementos no DOM
 const btnMenu = document.getElementById('btn-menu');
 const menuLateral = document.getElementById('menu-lateral');
 
 // Adiciona o evento de clique no botão de hambúrguer
 btnMenu.addEventListener('click', () => {
-    // Alterna a classe 'aberto' no menu lateral
     menuLateral.classList.toggle('aberto');
 });
